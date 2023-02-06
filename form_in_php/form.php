9b124577e5a938a3cef1b9857d83693057ee9524
LanaDantas <lanadantas@icloud.com>
2023-02-06 10:33:31 +0100

    ...

---
+++ form_in_php/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html>  
+<body>
+
+<form action="log-in-response.php" method="get">
+Name: <input type="text" name="first_name"><br>
+Cognome: <input type="text" name="last_name"><br>
+E-mail: <input type="text" name="email"><br>
+Password: <input type="password" value="segretissimo" name="pasword"><br>
+
+<input type="submit">
+</form>
+
+</body>
+</html>
+
---
+++ form_in_php/log-in-response.php
@@ -0,0 +1,7 @@
+<h1>Sono la risposta (RESPONSE)</h1>
+
+<?php
+
+echo "<pre>";
+print_r($_GET);
+echo "</pre>";
