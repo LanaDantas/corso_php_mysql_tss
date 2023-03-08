91f59570386e6c89cb44792f5d88cd8cb54be2d4
LanaDantas <lanadantas@icloud.com>
2023-03-08 12:11:37 +0100

    sql_province_sigle--update

--- basi_json/install.sql
+++ basi_json/install.sql
@@ -16,22 +16,3 @@ SELECT * from regione;
 TRUNCATE TABLE regione;
 
 insert into regione (nome) values('Valle d\'Aosta/Vallée d\'Aoste');
-
-CREATE TABLE provincia (
-    id_provincia int NOT NULL AUTO_INCREMENT,
-    nome VARCHAR(99) not NULL,
-    sigla CHAR(2) NOT NULL,
-    id_regione int,
-    PRIMARY KEY(id_provincia),
-    FOREIGN KEY (id_regione) REFERENCES regione(id_regione)
-);
-
-drop table provincia;
-
-INSERT INTO provincia(nome) VALUES('Agrigento');
-
-SELECT * FROM provincia;
-
-TRUNCATE TABLE provincia;
-
-INSERT INTO provincia (nome) VALUES ('Viterbo');
